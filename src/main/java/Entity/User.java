02858e5fb0e9fe9c5f23f04753028c10129706f6
PhillipWong0627 <phillipwong0627@gmail.com>
2023-02-07 04:04:59 +0800

    Created User Class

---
+++ src/main/java/Entity/User.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+/**
+ *
+ * @author user
+ */
+public class User {
+    private String userID;
+    private String password;
+    private String dateOfBirth;
+    private Integer phoneNumber;
+    
+    
+}
