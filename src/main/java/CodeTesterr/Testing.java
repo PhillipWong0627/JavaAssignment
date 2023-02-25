dae9b9c6418d28038eeedefa689eae7cdfe69bce
PhillipWong0627 <85791085+PhillipWong0627@users.noreply.github.com>
2023-02-25 22:39:00 +0800

    Tester Git

--- src/main/java/CodeTesterr/Testing.java
+++ src/main/java/CodeTesterr/Testing.java
@@ -12,6 +12,9 @@ public class Testing {
     
     public static void main(String [] args){
         System.out.println("Hello Worlds");
+        System.out.println("LONELY DOG");
+        
+        System.out.println("Fker");
     }
     
 }
